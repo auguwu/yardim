98dc54fdba1c8ac6416aac13d015bab96c13fe5c
Noel <august@augu.dev>
2021-01-30 19:18:51 -0700

    it needs porn duh:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // end my life please
 
 // ~ by August ~
-
+const {            get            } = require ( '@augu/orchid' );
 const { token } = require('./config.json');
 const { Client } = require('wumpcord');
 
@@ -13,6 +13,9 @@ const yardim = new Client({
                             intents: ['guilds', 'guildMessages']
   }
 });
+
+yardim
+                      .connect ( ) ;
 
 yardim
 
@@ -63,7 +66,140 @@ type: 2,
                         
           )
 
-yardim                             
+                                                                                                                                                    // use nino lol (https://nino.augu.dev <3)
 
-    
-            .connect()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+yardim . on ( 'message', event => { 
+
+
+          if (
+            
+            
+                event.message.content === '!help'
+                
+                
+                
+          )        {
+
+            // humanity is fucked when March 24th, 2004 at ??:?? - a fellow
+            // soon-to-be gay ass motherfucking furry appears in the real world
+
+                    event
+
+            .
+
+                  message
+
+            
+                  .
+                  
+                reply( 
+                  
+                  {
+
+                    content: 'fuck fuck fuck fuck it does `!help` `!yiff` `!ping`\n\n\n\n\n\n\n it also reacts to `bot bad` or `bad bot lol`!!!!!!!!!!!!!!!!'
+
+                  } 
+                  
+                  
+              )
+
+              
+          }
+
+} )
+
+
+
+
+yardim
+
+      . on ( 'message', async event =>      { 
+
+          if (event.message.content === '!yiff') 
+          
+          
+                  {
+
+/*                          if (
+                            
+                                    !   event
+                                    
+                                          .channel.
+                                          
+                                  nsfw
+                                  
+                            ) return event
+                              
+                                      .message
+                                      
+                                  .reply( 'BONK NO HORNY SA SA SA SA' 
+                                  
+                          );
+*/
+
+                      const res = await get(
+                        
+                        
+                                'https://api.floofy.dev/yiff'
+                                
+                                
+                      );
+
+                      const d = res.json(    );
+
+
+                      return event
+
+                      .               message
+
+                            . reply ( { 
+
+                                        embed: {
+
+                                          color: 0x696969,
+
+
+                                      image : {
+
+
+                                url: d.url
+
+
+                        }
+                                }
+                            
+                            } )
+
+                  }
+
+
+      }
+      
+      
+  )
